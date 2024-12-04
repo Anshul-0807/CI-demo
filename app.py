080d2c99d0d41629b9dfbd6d4f00f0c55196aab0
Anshul patle <patleanshul920@gmail.com>
2024-12-04 14:27:00 +0530

    fix linting issue

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ def sum(a,b):
     return a + b
 
 
-def sub(a,b) : return     a-b
+def sub(a,b):
+    return     a-b
 
 
-out1 = sum(4,6)
-out2 = sub(9,5)
+out1 = sum(4, 6)
+out2 = sub(9, 5)
 
 print(out1, out2)
